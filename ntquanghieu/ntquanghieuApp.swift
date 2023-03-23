a0621273a947aa7177031f51acc60fa0be696d96
‘GiangWeb’ <vanphonggiang@gmail.com>
2023-03-23 12:44:35 +0700

    Initial Commit

---
+++ ntquanghieu/ntquanghieuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ntquanghieuApp.swift
+//  ntquanghieu
+//
+//  Created by admin on 23/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ntquanghieuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
